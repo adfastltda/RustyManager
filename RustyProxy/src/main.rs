e76031625677f6cbf173c20bb00abccc0e3f7636
ADFAST LTDA <143732424+adfastltda@users.noreply.github.com>
2025-01-22 04:39:38 -0300

    Update main.rs

--- RustyProxy/src/main.rs
+++ RustyProxy/src/main.rs
@@ -129,7 +129,7 @@ fn get_port() -> u16 {
 
 fn get_status() -> String {
     let args: Vec<String> = env::args().collect();
-    let mut status = String::from("@RustyManager");
+    let mut status = String::from("@adfastltda");
 
     for i in 1..args.len() {
         if args[i] == "--status" {
